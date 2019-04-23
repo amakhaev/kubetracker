88b97eaac8defa7204c1681aec620968977522ed
RP\trofim.kaufmann <trofim.kaufmann@outlook.com>
2019-04-23 16:26:13 +0300

    Fixed sql script

--- src/main/resources/db/v1.4.sql
+++ src/main/resources/db/v1.4.sql
@@ -2,10 +2,12 @@ ALTER TABLE settings RENAME TO _settings_old;
 
 CREATE TABLE settings (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  kubernetes_name NOT NULL UNIQUE,
-  jenkins_name NOT NULL UNIQUE,
-  password NOT NULL UNIQUE,
-  jenkins_api_token NOT NULL UNIQUE VARCHAR(256)
+  kubernetes_name,
+  jenkins_name,
+  password,
+  jenkins_api_token VARCHAR(256),
+  unique (kubernetes_name, password),
+  unique (jenkins_name, jenkins_api_token)
 );
 
 INSERT INTO settings (kubernetes_name, jenkins_name, password)
