81fa69c484eac38cd174cadc5f439f0e6303238a
RP\trofim.kaufmann <trofim.kaufmann@outlook.com>
2019-04-24 17:57:06 +0300

    Fixed update token

--- src/main/java/com/tracker/domain/token/TokenServiceImpl.java
+++ src/main/java/com/tracker/domain/token/TokenServiceImpl.java
@@ -1,5 +1,6 @@
 package com.tracker.domain.token;
 
+import com.tracker.domain.common.exception.UnauthorizedException;
 import com.tracker.domain.settings.SettingService;
 import com.tracker.domain.settings.SettingsModel;
 import com.tracker.utils.GsonUtility;
@@ -18,6 +19,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,6 +35,10 @@ public class TokenServiceImpl implements TokenService {
     private final String kubeClientId;
     private final String kubeClientSecret;
     private final SettingService settingService;
+    private TokenModel tokenModel;
+    private LocalTime tokenLastUpdate;
+    private LocalTime tokenCreatedAt;
+
 
     /**
      * Initialize new instance of {@link TokenServiceImpl}
@@ -45,7 +52,6 @@ public class TokenServiceImpl implements TokenService {
         this.kubeClientId = kubeClientId;
         this.kubeClientSecret = kubeClientSecret;
         this.settingService = settingService;
-        this.getToken();
     }
 
     /**
@@ -54,7 +60,14 @@ public class TokenServiceImpl implements TokenService {
     @Override
     public synchronized TokenModel getToken() {
         SettingsModel settingsModel = this.settingService.getSettings();
-        return this.makeTokenRequest(settingsModel);
+
+        if (this.tokenModel == null || this.isNeedToReLogin()) {
+            return this.makeTokenRequest(settingsModel);
+        } else if (this.isNeedToRefresh()) {
+            return this.makeRefreshTokenRequest(settingsModel, this.tokenModel.getRefreshToken());
+        } else {
+            return this.tokenModel;
+        }
     }
 
     private TokenModel makeTokenRequest(SettingsModel setting) {
@@ -81,13 +94,81 @@ public class TokenServiceImpl implements TokenService {
             if (result.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
                 log.info("Token retrieved successfully");
             } else {
-                log.error("Token retrieved with error. Response: {}", json);
+                throw new UnauthorizedException("Token retrieved with error. Response: " + json);
             }
-            return GsonUtility.parse(json, TokenModel.class);
+            this.tokenModel = GsonUtility.parse(json, TokenModel.class);
+            this.tokenLastUpdate = LocalTime.now();
+            this.tokenCreatedAt = LocalTime.now();
+            return this.tokenModel;
 
         } catch (IOException ex) {
             log.error(ex.getMessage());
             return null;
         }
     }
+
+    private TokenModel makeRefreshTokenRequest(SettingsModel setting, String refreshToken) {
+        if (refreshToken == null || refreshToken.isEmpty()) {
+            return this.makeTokenRequest(setting);
+        }
+
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+            log.debug("Try to refresh token");
+
+            HttpPost request = new HttpPost(this.authUrl);
+
+            List<NameValuePair> postParameters = new ArrayList<>();
+            postParameters.add(new BasicNameValuePair("grant_type", "refresh_token"));
+            postParameters.add(new BasicNameValuePair("client_id", this.kubeClientId));
+            postParameters.add(new BasicNameValuePair("client_secret", this.kubeClientSecret));
+            postParameters.add(new BasicNameValuePair("scope", "openid"));
+            postParameters.add(new BasicNameValuePair("refresh_token", refreshToken));
+
+            request.setEntity(new UrlEncodedFormEntity(postParameters, "UTF-8"));
+
+            HttpResponse result = httpClient.execute(request);
+            String json = EntityUtils.toString(result.getEntity(), "UTF-8");
+
+            if (result.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                log.info("Token refreshed successfully");
+            } else if (result.getStatusLine().getStatusCode() == HttpStatus.SC_UNAUTHORIZED) {
+                log.debug("Can't refresh token. Try to re-login");
+                return this.makeTokenRequest(setting);
+            } else {
+                log.error("Token refreshed with error. Response: ");
+                log.error(json);
+            }
+            this.tokenModel = GsonUtility.parse(json, TokenModel.class);
+            this.tokenLastUpdate = LocalTime.now();
+            return this.tokenModel;
+
+        } catch (IOException ex) {
+            log.error(ex.getMessage());
+            return null;
+        }
+    }
+
+    private boolean isNeedToReLogin() {
+        return this.tokenCreatedAt == null ||
+                this.tokenModel == null ||
+                ChronoUnit.HOURS.between(LocalTime.now(), this.tokenCreatedAt) >= 10;
+
+    }
+
+    private boolean isNeedToRefresh() {
+        if (this.tokenLastUpdate == null || this.tokenModel == null) {
+            return true;
+        }
+
+        int expiredTimeInSecond;
+        try {
+            expiredTimeInSecond = Integer.parseInt(this.tokenModel.getExpiresIn());
+        } catch (NumberFormatException e) {
+            log.error("Can't parse token expiration time as integer. Current value: " + this.tokenModel.getExpiresIn());
+            return true;
+        }
+
+        return ChronoUnit.SECONDS.between(this.tokenLastUpdate, LocalTime.now()) >= expiredTimeInSecond - 5;
+    }
+
 }
